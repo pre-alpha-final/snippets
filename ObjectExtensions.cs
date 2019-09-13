5874d77bfa35c987afb5909a8c7c4674e86bc11d
spanishprisoner <jacek.mowinski@gmail.com>
2019-09-13 11:08:31 +0200

    Adding ObjectExtensions

---
+++ ObjectExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Globalization;
+
+namespace Namespace
+{
+	public static class ObjectExtensions
+	{
+		public static string ToInvariantString(this object obj)
+		{
+			return obj is IConvertible convertible ? convertible.ToString(CultureInfo.InvariantCulture)
+				: obj is IFormattable formattable ? formattable.ToString(null, CultureInfo.InvariantCulture)
+				: obj.ToString();
+		}
+	}
+}
