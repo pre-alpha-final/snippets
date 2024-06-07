d7ddf455ff65fa88da1aa16000bfa7a6b60e3511
pre-alpha-final <jacek.mowinski@gmail.com>
2024-06-07 19:07:33 +0200

    Adding debouncer

---
+++ Debouncer.cs
@@ -0,0 +1,14 @@
+﻿public class Debouncer
+{
+    private Func<Task>? _func = null;
+
+    public async Task Run(Func<Task> func, int delay = 100)
+    {
+        _func = func;
+        await Task.Delay(delay);
+        if (_func == func)
+        {
+            await func();
+        }
+    }
+}
